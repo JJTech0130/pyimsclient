d208eb2aa500efdb808b1d0cb247c2b714a4bf71
James Gill <jjtech@jjtech.dev>
2025-11-07 14:02:24 -0500

    tun: cleanup Linux implementation fix routing issues, just shell out to ip

--- ipsec/tun.py
+++ ipsec/tun.py
@@ -24,217 +24,74 @@ class Tun:
         pass
 
     def fileno(self) -> int:
+        # Return the file descriptor number for use with select()
+        # This should probably be removed at some point
         raise NotImplementedError
 
 class TunLinux(Tun):
-    TUN_DEVICE = "/dev/net/tun" # from <linux/if_tun.h>
-    IFF_TUN = 0x0001
-    IFF_NO_PI = 0x1000
-    IFF_UP = 0x1
-    IFF_RUNNING = 0x40
-    TUNSETIFF = 0x400454ca  # _IOW('T', 202, int) - common on Linux
-    IFNAMSIZ = 16
-    SIOCSIFFLAGS = 0x8914
-    SIOCSIFADDR = 0x8916  # from <linux/sockios.h>
-    SIOCSIFNETMASK = 0x891c
+    TUNSETIFF = 0x400454ca  # _IOW('T', 202, int) from <linux/if_tun.h>
+    IFF_TUN = 0x0001        #                     from <linux/if_tun.h>
+    IFF_NO_PI = 0x1000      #                     from <linux/if_tun.h>
+    IFNAMSIZ = 16           #                     from <linux/if.h>
 
     def __init__(self):
         super().__init__()
-        # open /dev/net/tun
-        fd = os.open(self.TUN_DEVICE, os.O_RDWR)
-        self.fd = fd
 
-        # we use empty name hint to get tunX automatically
-        # ifreq: char ifr_name[IFNAMSIZ]; short ifr_flags; rest unused
-        ifreq = b"\x00" * self.IFNAMSIZ + (self.IFF_TUN | self.IFF_NO_PI).to_bytes(2, "little") + b"\x00" * 14
+        # interfaces created with /dev/net/tun are automatically removed when the program exits
+        self._fd = os.open("/dev/net/tun", os.O_RDWR)
+
+        # we use empty ifr_name hint to get tunX automatically
+        # from <linux/if.h>
+        # struct ifreq: char ifr_name[IFNAMSIZ]; short ifr_flags; rest unused
+        ifreq = bytearray(b"\x00" * self.IFNAMSIZ + (self.IFF_TUN | self.IFF_NO_PI).to_bytes(2, "little") + b"\x00" * 14)
         
-        # ioctl to create
-        try:
-            res = fcntl.ioctl(self.fd, self.TUNSETIFF, ifreq)
-        except OSError:
-            os.close(self.fd)
-            raise
-        # the kernel returns the actual interface name in the ifreq
-        self._ifname = res[:self.IFNAMSIZ].split(b'\x00', 1)[0].decode()
+        fcntl.ioctl(self._fd, self.TUNSETIFF, ifreq)
+        self._ifname = ifreq[:self.IFNAMSIZ].split(b'\x00', 1)[0].decode()
 
-        # hack: disable automatic IPv6 address generation on the interface
-        # (to make behavior similar to macOS utun)
-        # equivalent: ip link set dev <ifname> addrgenmode none
-        addr_gen_path = f"/proc/sys/net/ipv6/conf/{self._ifname}/addr_gen_mode"
-        try:
-            with open(addr_gen_path, "w") as f:
-                f.write("1\n")
-        except Exception:
-            pass  # not critical
+        # hack: disable SLAAC/accepting RAs over the tunnel
+        open(f"/proc/sys/net/ipv6/conf/{self._ifname}/accept_ra", "w").write("0\n")
 
         # bring up the interface
-        # equivalent: ip link set dev <ifname> up
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # ifreq = char name[16]; short flags; 14 padding
-        ifname_bytes = self._ifname.encode("utf-8").ljust(self.IFNAMSIZ, b"\x00")
-        flags_bytes = (self.IFF_UP | self.IFF_RUNNING).to_bytes(2, "little")
-        ifreq = ifname_bytes + flags_bytes + b"\x00" * 14
-        fcntl.ioctl(s, self.SIOCSIFFLAGS, ifreq)
+        subprocess.run(["/sbin/ip", "link", "set", "dev", self._ifname, "up"])
 
-        print(f"Initialized Linux TUN: {self._ifname} (fd={self.fd})")
+        print(f"Initialized Linux TUN: {self._ifname} (fd={self._fd})")
 
     def write_packet(self, packet: bytes):
-        # Linux tun device expects pure IP packet (no extra header if IFF_NO_PI)
-        os.write(self.fd, packet)
-        # optional: return number of bytes written
-        return
+        os.write(self._fd, packet)
 
     def read_packet(self, size: int = 65536) -> bytes:
-        return os.read(self.fd, size)
+        return os.read(self._fd, size)
 
     def ifname(self) -> str:
         return self._ifname
     
-    def _add_address_netlink(self, ifname: str, addr: str, prefixlen: int):
-        RTM_NEWADDR = 20
-        NLM_F_REQUEST = 1
-        NLM_F_ACK = 4
-        NLM_F_CREATE = 0x400
-        NLM_F_EXCL = 0x200
-        AF_INET6 = socket.AF_INET6
-        NETLINK_ROUTE = 0
-        AF_NETLINK = 16
-
-        IFA_ADDRESS = 1
-        IFA_LOCAL = 2
-
-        s = socket.socket(AF_NETLINK, socket.SOCK_RAW, NETLINK_ROUTE)
-        pid = os.getpid()
-        ifindex = socket.if_nametoindex(ifname)
-
-        ip_bytes = socket.inet_pton(AF_INET6, addr)
-
-        def nlattr(attr_type, payload):
-            length = len(payload) + 4
-            pad = (4 - (length % 4)) % 4
-            return (
-                length.to_bytes(2, "little") +
-                attr_type.to_bytes(2, "little") +
-                payload +
-                b"\x00" * pad
-            )
-
-        ifa_family = AF_INET6
-        ifa_prefixlen = prefixlen
-        ifa_flags = 0
-        ifa_scope = 0
-        ifa_index = ifindex
-
-        ifa_hdr = (
-            ifa_family.to_bytes(1, "little") +
-            ifa_prefixlen.to_bytes(1, "little") +
-            ifa_flags.to_bytes(1, "little") +
-            ifa_scope.to_bytes(1, "little") +
-            ifa_index.to_bytes(4, "little")
-        )
-
-        attrs = nlattr(IFA_ADDRESS, ip_bytes) + nlattr(IFA_LOCAL, ip_bytes)
-        payload = ifa_hdr + attrs
-
-        # ---- Netlink header ----
-        nlmsg_len = 16 + len(payload)
-        nlmsg_type = RTM_NEWADDR
-        nlmsg_flags = NLM_F_REQUEST | NLM_F_ACK | NLM_F_EXCL | NLM_F_CREATE
-        nlmsg_seq = 1
-        nlmsg_pid = pid
-
-        nlhdr = (
-            nlmsg_len.to_bytes(4, "little") +
-            nlmsg_type.to_bytes(2, "little") +
-            nlmsg_flags.to_bytes(2, "little") +
-            nlmsg_seq.to_bytes(4, "little") +
-            nlmsg_pid.to_bytes(4, "little")
-        )
-
-        msg = nlhdr + payload
-        s.send(msg)
-
-        try:
-            resp = s.recv(8192)
-            offset = 0
-            while offset < len(resp):
-                # Netlink header: 16 bytes
-                if len(resp) - offset < 16:
-                    print("Incomplete netlink header")
-                    break
-
-                nlmsg_len = int.from_bytes(resp[offset:offset+4], "little")
-                nlmsg_type = int.from_bytes(resp[offset+4:offset+6], "little")
-                nlmsg_flags = int.from_bytes(resp[offset+6:offset+8], "little")
-                nlmsg_seq = int.from_bytes(resp[offset+8:offset+12], "little")
-                nlmsg_pid = int.from_bytes(resp[offset+12:offset+16], "little")
-
-                payload = resp[offset+16:offset+nlmsg_len]
-
-                # Check for ACK or error
-                if nlmsg_type == 2:  # NLMSG_ERROR
-                    if len(payload) >= 4:
-                        error_code = int.from_bytes(payload[:4], "little", signed=True)
-                        if error_code == 0:
-                            print("ACK received")
-                        else:
-                            print(f"Netlink error: {error_code}")
-                    else:
-                        print("Malformed NLMSG_ERROR")
-                else:
-                    print(f"Message type {nlmsg_type}, length {nlmsg_len}")
-
-                # Move to next message
-                offset += nlmsg_len
-                if nlmsg_len % 4 != 0:
-                    offset += 4 - (nlmsg_len % 4)  # 4-byte alignment
-        except socket.error as e:
-            print("Socket error:", e)
-        finally:
-            s.close()
-    
     def add_address(self, addr: str, ipv6: bool = False):
-        if '/' in addr:
-            ip, prefixlen_str = addr.split('/', 1)
-            prefixlen = int(prefixlen_str)
-        else:
-            ip = addr
-            prefixlen = 64
+        # Hack: shell out to ip, replace this with netlink in the future?
         if ipv6:
-            self._add_address_netlink(self._ifname, ip, prefixlen)
+            subprocess.run(["/sbin/ip", "-6", "addr", "add", addr, "dev", self._ifname])
+            # Hack: IPv6 won't work on the tunnel interface without a default route, use a high metric to avoid interfering with normal routing
+            subprocess.run(["/sbin/ip", "-6", "route", "add", "default", "dev", self._ifname, "metric", "20000"])
         else:
-            if prefixlen == 64:
-                prefixlen = 32
-
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            ifreq = self._ifname.encode('utf-8').ljust(16, b'\x00')[:16] + socket.AF_INET.to_bytes(2, 'little') + b'\x00'*2 + socket.inet_aton(ip) + b'\x00'*8
-            mask = (0xffffffff << (32 - prefixlen)) & 0xffffffff
-            ifreq_mask = self._ifname.encode('utf-8').ljust(16, b'\x00')[:16] + socket.AF_INET.to_bytes(2, 'little') + b'\x00'*2+ mask.to_bytes(4, 'big') + b'\x00'*8
-            fcntl.ioctl(s, self.SIOCSIFADDR, ifreq)
-            fcntl.ioctl(s, self.SIOCSIFNETMASK, ifreq_mask)
+            subprocess.run(["/sbin/ip", "-4", "addr", "add", addr, "dev", self._ifname])
 
     def close(self):
-        try:
-            os.close(self.fd)
-        except Exception:
-            pass
+        os.close(self._fd)
 
     def fileno(self) -> int:
-        return self.fd
+        return self._fd
 
 class TunMacOS(Tun):
     CTLIOCGINFO = 0xC0644E03 # _IOWR('N', 3, struct ctl_info) from <sys/kern_control.h>
     SIOCSIFADDR = 0x8020690  # _IOW('i', 12, struct ifreq)    from <sys/sockio.h>
     SYSPROTO_CONTROL = 2     #                                from <sys/sys_domain.h>
     UTUN_OPT_IFNAME = 2      #                                from <net/if_utun.h>
+    AF_SYSTEM = 32           #                                from <sys/socket.h>
 
 
     def __init__(self):
         super().__init__()
-        if not sys.platform == "darwin":
-            raise RuntimeError("TunMacOS can only be used on macOS")
         
-        s = socket.socket(socket.AF_SYSTEM, socket.SOCK_DGRAM, self.SYSPROTO_CONTROL)
+        self._sock = socket.socket(self.AF_SYSTEM, socket.SOCK_DGRAM, self.SYSPROTO_CONTROL)
 
         # from <sys/kern_control.h>
         # struct ctl_info { 
@@ -244,39 +101,36 @@ class TunMacOS(Tun):
         ctl_info = bytearray(b"\x00" * 4 + b"com.apple.net.utun_control".ljust(96, b"\x00"))
 
         # get the Controller ID for utun_control
-        fcntl.ioctl(s, self.CTLIOCGINFO, ctl_info, True)
+        fcntl.ioctl(self._sock, self.CTLIOCGINFO, ctl_info, True)
         ctl_id = int.from_bytes(ctl_info[:4], "little")
 
         # connecting to the control creates a new utun interface
         # 0 means the kernel chooses the next available utunX
-        s.connect((ctl_id, 0))
+        self._sock.connect((ctl_id, 0))
 
         # get the assigned interface name
-        ifname = s.getsockopt(self.SYSPROTO_CONTROL, self.UTUN_OPT_IFNAME, 32)
+        ifname = self._sock.getsockopt(self.SYSPROTO_CONTROL, self.UTUN_OPT_IFNAME, 32)
         self._ifname = ifname.split(b'\x00', 1)[0].decode()
 
-        self._sock = s
-        print(f"Initialized macOS utun: {self._ifname} (fd={s.fileno()})")
+        print(f"Initialized macOS utun: {self._ifname} (fd={self._sock.fileno()})")
 
     def add_address(self, addr: str, ipv6: bool = False):
-        # Hack: shell out to ifconfig, replace this with ioctl in the future
+        # Hack: shell out to ifconfig, replace this with ioctl in the future?
         # Remove the netmask from the second occurence ("dest", but we don't really use it that way)
         ip = addr.split("/", 1)[0]
         if ipv6:
             subprocess.run(["/sbin/ifconfig", self._ifname, "inet6", addr, "alias"])
             # Hack: add default IPv6 link-local route
-            # Equivalent: route add -inet6 -ifscope utun15 default fe80::%utun15
             subprocess.run(["/sbin/route", "add", "-inet6", "-ifscope", self._ifname, "default", f"fe80::%{self._ifname}"])
         else:
+            # Hack: use IPv4 "point-to-point" address (shows up as "addr -> ip"), dest doesn't actually matter
             subprocess.run(["/sbin/ifconfig", self._ifname, "inet", addr, ip, "alias"])
             # Hack: add default IPv4 route
-            # Equivalent: route add default -ifscope utun15 10.130.184.132
             subprocess.run(["/sbin/route", "add", "default", "-ifscope", self._ifname, ip])
 
 
     def write_packet(self, packet: bytes):
         # utun expects a 4-byte network-order family header before the packet bytes.
-        # For IPv4: socket.AF_INET (2); IPv6: socket.AF_INET6 (10). We'll assume IPv6 if packet starts with 0x6.
         fam = socket.AF_INET6 if packet and (packet[0] >> 4) == 6 else socket.AF_INET
         hdr = fam.to_bytes(4, "big")
         self._sock.sendall(hdr + packet)
@@ -298,9 +152,6 @@ class TunMacOS(Tun):
         return self._sock.fileno()
 
 def open_tun() -> Tun:
-    """
-    Open a platform-appropriate TUN/UTUN and return a Tun-derived object.
-    """
     if sys.platform.startswith("linux"):
         return TunLinux()
     elif sys.platform == "darwin":
--- test_iface.py
+++ test_iface.py
@@ -2,14 +2,16 @@ import socket
 import sys
 
 def bind_socket_to_iface(sock: socket.socket, ifname: str):
-    ifindex = socket.if_nametoindex(ifname)
-    level = socket.IPPROTO_IPV6 if sock.family == socket.AF_INET6 else socket.IPPROTO_IP
-    optname = 125 if sock.family == socket.AF_INET6 else 25 # IPV6_BOUND_IF / IP_BOUND_IF
-    sock.setsockopt(level, optname, ifindex)
-    
-    
-def send_http_request(interface: str, host: str = "api64.ipify.org", port: int = 80):
-    with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
+    if sys.platform == "darwin":
+        ifindex = socket.if_nametoindex(ifname)
+        level = socket.IPPROTO_IPV6 if sock.family == socket.AF_INET6 else socket.IPPROTO_IP
+        optname = 125 if sock.family == socket.AF_INET6 else 25 # IPV6_BOUND_IF / IP_BOUND_IF
+        sock.setsockopt(level, optname, ifindex)
+    elif sys.platform.startswith("linux"):
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, ifname.encode())
+
+def send_http_request(interface: str, host: str, port: int = 80, type: int = socket.AF_INET):
+    with socket.socket(type, socket.SOCK_STREAM) as s:
         bind_socket_to_iface(s, interface)
 
         print(f"[+] Connecting to {host}:{port} ...")
@@ -31,5 +33,13 @@ if __name__ == "__main__":
         sys.exit(1)
 
     interface = sys.argv[1]
-    send_http_request(interface)
+    try:
+        send_http_request(interface, "api.ipify.org", type=socket.AF_INET)
+    except Exception as e:
+        print(f"IPv4 doesn't work")
+    print()
+    try:
+        send_http_request(interface, "api64.ipify.org", type=socket.AF_INET6)
+    except Exception as e:
+        print(f"IPv6 doesn't work")
     print()
